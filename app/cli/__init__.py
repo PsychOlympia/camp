92164cca0b070aeb76fe8201c38fc0e825c21501
Felix <felix-zenk@web.de>
2023-06-16 20:09:05 +0200

    Restructured cli

--- app/cli/__init__.py
+++ app/cli/__init__.py
@@ -1,9 +1,11 @@
-from flask import Blueprint
+from flask import Flask, Blueprint
 
 from .database import database
 from .scoreboard import scoreboard
 from .teams import teams
 from .users import users
+from .translate import translate
+
 
 bp_psy = Blueprint('psy', __name__)
 bp_psy.cli.help = 'PyschOlympia related commands'
@@ -11,3 +13,8 @@ bp_psy.cli.add_command(database)
 bp_psy.cli.add_command(scoreboard)
 bp_psy.cli.add_command(teams)
 bp_psy.cli.add_command(users)
+
+
+def cli_init_app(app: Flask) -> None:
+    app.register_blueprint(bp_psy)
+    app.cli.add_command(translate)
