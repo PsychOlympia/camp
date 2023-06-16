a4c260aa0964504be25021432abc367297f8f7fd
Felix <felix-zenk@web.de>
2023-06-16 20:10:05 +0200

    Add LoginForm

---
+++ app/auth/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from flask_babel import lazy_gettext as _l
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField(_l('Username'), validators=[DataRequired()])
+    password = PasswordField(_l('Password'), validators=[DataRequired()])
