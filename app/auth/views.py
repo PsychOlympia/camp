6fdba4059c143ec9fd52843cfb339fb5ba64b1bd
felix-zenk <felix.zenk@web.de>
2023-06-18 19:07:34 +0200

    Remember user logins

--- app/auth/views.py
+++ app/auth/views.py
@@ -17,7 +17,7 @@ def login():
         if form.validate_on_submit():
             user = db.session.query(User).where(User.username == form.username.data).first()
             if user is not None and bcrypt.check_password_hash(user.password_hash, form.password.data):
-                login_user(user)
+                login_user(user, remember=True)
                 identity_changed.send(current_app, identity=Identity(user.id))
                 # TODO redirect to a personal page after login?
                 return redirect(next_url(default=url_for('main.index')))
