bb637f0876f83e4f9dd742ae06b2110c8ac50e31
felix-zenk <felix.zenk@web.de>
2023-06-19 10:29:23 +0200

    Remove User.locale

--- app/i18n.py
+++ app/i18n.py
@@ -2,14 +2,9 @@ from __future__ import annotations
 
 from flask import request, current_app
 from flask_babel import Babel
-from flask_login import current_user
 
 
 def locale_selector() -> str | None:
-    if current_user.is_authenticated:
-        if current_user.locale in current_app.config['BABEL_SUPPORTED_LOCALES']:
-            return current_user.locale
-
     return request.accept_languages.best_match(
         current_app.config['BABEL_SUPPORTED_LOCALES'],
         current_app.config['BABEL_DEFAULT_LOCALE']
