b17d0cafa6217ca284ab6ef38f152a23d6c8884e
felix <felix.zenk@axians.de>
2023-06-22 00:59:43 +0200

    Added database init command

--- app/cli/database.py
+++ app/cli/database.py
@@ -1,6 +1,10 @@
 import click
+from click import Context
+from flask import current_app
+from sqlalchemy.exc import IntegrityError
 
-from ..models import db
+from ..auth import RoleName
+from ..models import db, PointOfInterest, serialize_coordinates
 
 
 @click.group('database', help='Manage the database')
@@ -22,3 +26,26 @@ def reset(confirm: bool):
         click.secho('The database has been reset!', fg='bright_green')
     except Exception as e:
         click.secho(f'Error {e.__class__.__name__}: {e}', fg='bright_red')
+
+
+@database.command('init', help='Create the minimal needed objects')
+@click.pass_context
+def init(ctx: Context):
+    from .users import add_user
+    ctx.invoke(add_user, username='root', user_roles=tuple(member.value for member in RoleName.__members__.values()))
+    camp_location = serialize_coordinates((
+        current_app.config['CAMP_LOCATION_LAT'], current_app.config['CAMP_LOCATION_LON']
+    ))
+    db.session.add(PointOfInterest(
+        name='PsychOlympia',
+        _camp_location=camp_location,
+        _country_location=camp_location,
+        logo='psycholympia.png',
+        linkable=False
+    ))
+    try:
+        db.session.commit()
+        click.secho("Added POI 'PsychOlympia' to the database.", fg='bright_green')
+    except IntegrityError:
+        click.secho("The database already contains the POI 'PsychOlympia'", fg='bright_red')
+    click.secho('Initialized database', fg='bright_green')
