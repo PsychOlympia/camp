086605b507ffc451551afc0ee5b30497c2e0b30e
felix <felix.zenk@axians.de>
2023-06-22 01:21:38 +0200

    Add maps

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,12 @@
+from http import HTTPStatus
+
 from flask import Blueprint, render_template, Response
+from flask_login import login_required
+
+from ..auth import team_permission
+from ..models import db, Team, PointOfInterest, IsMapItem
 
 bp_main = Blueprint('main', __name__, template_folder='templates', static_folder='static')
-
-
-@bp_main.route('/', methods=['GET'], endpoint='index')
-def index():
-    return render_template('index.jinja2')
 
 
 @bp_main.route('/favicon.ico', defaults={'size': 192}, methods=['GET'])
@@ -17,3 +18,30 @@ def favicon(size: int):
 @bp_main.route('/robots.txt', methods=['GET'])
 def robots():
     return Response(response='User-agent: *\nDisallow: /', mimetype='text/plain')
+
+
+@bp_main.route('/', methods=['GET'], endpoint='index')
+def index():
+    return render_template('index.jinja2')
+
+
+@bp_main.route('/team-map', methods=['GET'], endpoint='team_map')
+@login_required
+@team_permission.require(http_exception=HTTPStatus.UNAUTHORIZED)
+def index():
+    map_items: list[IsMapItem] = (
+        db.session.query(Team).where(Team._country_location != None).all()  # noqa
+        + db.session.query(PointOfInterest).where(PointOfInterest._country_location != None).all()  # noqa
+    )
+    return render_template('team_map.jinja2', map_items=map_items)
+
+
+@bp_main.route('/camp-map', methods=['GET'], endpoint='camp_map')
+@login_required
+@team_permission.require(http_exception=HTTPStatus.UNAUTHORIZED)
+def index():
+    map_items: list[IsMapItem] = (
+        db.session.query(Team).where(Team._camp_location != None).all()  # noqa
+        + db.session.query(PointOfInterest).where(PointOfInterest._camp_location != None).all()  # noqa
+    )
+    return render_template('camp_map.jinja2', map_items=map_items)
