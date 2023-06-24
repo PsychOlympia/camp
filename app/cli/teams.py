3d727bf71444d6e1a6f8d98a362cb98546e79fde
felix <felix.zenk@axians.de>
2023-06-25 01:42:19 +0200

    Fix duplicate newline

--- app/cli/teams.py
+++ app/cli/teams.py
@@ -115,7 +115,7 @@ def import_teams(csv_file: str, template: bool):
     if template:
         click.secho(f'Writing template to to {csv_file}', fg='bright_blue')
         with open(csv_file, mode='w') as f:
-            writer = csv.writer(f)
+            writer = csv.writer(f, lineterminator='\n')
             writer.writerows([
                 ('Teamname', 'Team accent-color', 'Team category', 'Camp location', 'Country location', 'Team member 1 name', 'Team member 2 name', '...'),
                 ('Testteam', '#0000ff', 'team', '51.165,10.455278', None, 'user1', 'user2', 'user3', 'user4')
