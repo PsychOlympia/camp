9e82d3376ae0cfca7050c946105cd0601e88a81b
felix <felix.zenk@axians.de>
2023-06-25 01:30:46 +0200

    Add scoreboard entries

--- app/scoreboard/views.py
+++ app/scoreboard/views.py
@@ -2,8 +2,10 @@ from http import HTTPStatus
 
 from flask import Blueprint, render_template
 from flask_login import login_required
+from sqlalchemy import desc
 
-from app.auth import team_permission
+from ..auth import team_permission
+from ..models import db, Scoreboard
 
 bp_scoreboard = Blueprint(
     'scoreboard', __name__, template_folder='templates', static_folder='static', url_prefix='/scoreboard'
@@ -14,4 +16,9 @@ bp_scoreboard = Blueprint(
 @login_required
 @team_permission.require(http_exception=HTTPStatus.UNAUTHORIZED)
 def index():
-    return render_template('scoreboard.jinja2')
+    latest_round = db.session.query(Scoreboard).order_by(desc(Scoreboard.round)).first()
+    if latest_round is None:
+        entries = []
+    else:
+        entries = latest_round.entries
+    return render_template('scoreboard.jinja2', entries=entries)
