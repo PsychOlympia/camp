f84356b1cbf263c711dad74a1b9051c3d2ae2d74
felix <felix.zenk@axians.de>
2023-06-24 18:53:08 +0200

    Update profile endpoints

--- app/profile/views.py
+++ app/profile/views.py
@@ -5,38 +5,38 @@ from flask_babel import gettext as _
 from flask_login import login_required
 
 from ..auth import team_permission, guest_permission
-from ..models import db, Team
+from ..models import db, Team, User
 
 bp_profile = Blueprint('profile', __name__, template_folder='templates', static_folder='static', url_prefix='/profile')
 
 
-@bp_profile.route('/', methods=['GET'], endpoint='index')
+@bp_profile.route('/', methods=['GET'], endpoint='my_profile')
 @login_required
 @guest_permission.require(http_exception=HTTPStatus.UNAUTHORIZED)
-def index():
+def my_profile():
     return render_template('my_profile.jinja2')
 
 
-@bp_profile.route('/team-profile', methods=['GET'], endpoint='team_profile')
+@bp_profile.route('/profile', methods=['GET'], endpoint='user_profile')
 @login_required
-@team_permission.require(http_exception=HTTPStatus.UNAUTHORIZED)
-def team_profile():
-    team_name = request.args.get('team')
-    if team_name is None:
-        flash(_('No team name provided!'), 'danger')
+@guest_permission.require(http_exception=HTTPStatus.UNAUTHORIZED)
+def user_profile():
+    name = request.args.get('name')
+    if name is None:
+        flash(_('No name provided!'), 'danger')
         return redirect(url_for('main.index'))
-    team = db.session.query(Team).where(Team.name == team_name).first()
-    if team is None:
-        flash(_('The team %(team_name)s was not found!', team_name=team_name), 'danger')
+    user = db.session.query(User).where(User.username == name).first()
+    if user is None:
+        flash(_('The user %(username)s was not found!', username=name), 'danger')
         return redirect(url_for('main.index'))
-    return render_template('team_profile.jinja', team=team)
+    return render_template('user_profile.jinja2', team=user)
 
 
 @bp_profile.route('/<string:team_name>', methods=['GET'], endpoint='view')
 @login_required
 @team_permission.require(HTTPStatus.UNAUTHORIZED)
-def index(team_name: str):
+def team_profile(team_name: str):
     team = db.session.query(Team).where(Team.name == team_name).first()
     if team is None:
         abort(HTTPStatus.NOT_FOUND)
-    return render_template('profile.jinja2', team=team)
+    return render_template('team_profile.jinja2', team=team)
