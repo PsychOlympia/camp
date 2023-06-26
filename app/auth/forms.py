fc35b745ed41e47ad78e4c1045f50e7278468b76
felix <felix.zenk@axians.de>
2023-06-26 17:25:40 +0200

    Ask browser not to remember usernames

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -5,5 +5,5 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField(_l('Username'), validators=[DataRequired()])
+    username = StringField(_l('Username'), validators=[DataRequired()], render_kw={'autocomplete': 'off'})
     password = PasswordField(_l('Password'), validators=[DataRequired()])
