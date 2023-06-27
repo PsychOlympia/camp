4ae890540c90eca34afb6809ac13f9bbdf5f7bc6
felix-zenk <felix.zenk@web.de>
2023-06-27 11:01:34 +0200

    Move mkdir

--- app/uploads/views.py
+++ app/uploads/views.py
@@ -10,39 +10,64 @@ bp_uploads = Blueprint('upload', __name__, url_prefix='/uploads', static_folder=
 
 
 def get_user_upload_directory() -> Path:
-    return Path(bp_uploads.static_folder) / 'users'
+    path = Path(bp_uploads.static_folder) / 'users'
+    path.mkdir(parents=True, exist_ok=True)
+    return path
 
 
 def get_team_upload_directory() -> Path:
-    return Path(bp_uploads.static_folder) / 'teams'
+    path = Path(bp_uploads.static_folder) / 'teams'
+    path.mkdir(parents=True, exist_ok=True)
+    return path
 
 
 def get_poi_upload_directory() -> Path:
-    return Path(bp_uploads.static_folder) / 'pois'
+    path = Path(bp_uploads.static_folder) / 'pois'
+    path.mkdir(parents=True, exist_ok=True)
+    return path
+
+
+def get_temp_upload_directory() -> Path:
+    path = Path(bp_uploads.static_folder) / 'temp'
+    path.mkdir(parents=True, exist_ok=True)
+    return path
 
 
 @bp_uploads.route('/users/<string:filename>', methods=['GET'], endpoint='user')
 @login_required
 @guest_permission.require(http_exception=HTTPStatus.UNAUTHORIZED)
 def user_upload(filename: str):
-    user_upload_directory = get_user_upload_directory()
-    user_upload_directory.mkdir(parents=True, exist_ok=True)
-    return send_from_directory(directory=user_upload_directory.relative_to(Path(current_app.root_path)), path=filename)
+    return send_from_directory(
+        directory=get_user_upload_directory().relative_to(Path(current_app.root_path)),
+        path=filename
+    )
 
 
 @bp_uploads.route('/teams/<string:filename>', methods=['GET'], endpoint='team')
 @login_required
 @guest_permission.require(http_exception=HTTPStatus.UNAUTHORIZED)
 def team_upload(filename: str):
-    team_upload_directory = get_team_upload_directory()
-    team_upload_directory.mkdir(parents=True, exist_ok=True)
-    return send_from_directory(directory=team_upload_directory.relative_to(Path(current_app.root_path)), path=filename)
+    return send_from_directory(
+        directory=get_team_upload_directory().relative_to(Path(current_app.root_path)),
+        path=filename
+    )
 
 
 @bp_uploads.route('/point-of-interest/<string:filename>', methods=['GET'], endpoint='pointofinterest')
 @login_required
 @guest_permission.require(http_exception=HTTPStatus.UNAUTHORIZED)
-def team_upload(filename: str):
-    poi_upload_directory = get_poi_upload_directory()
-    poi_upload_directory.mkdir(parents=True, exist_ok=True)
-    return send_from_directory(directory=poi_upload_directory.relative_to(Path(current_app.root_path)), path=filename)
+def poi_upload(filename: str):
+    return send_from_directory(
+        directory=get_poi_upload_directory().relative_to(Path(current_app.root_path)),
+        path=filename
+    )
+
+
+@bp_uploads.route('/temp/<string:filename>', methods=['GET'], endpoint='pointofinterest')
+@login_required
+@guest_permission.require(http_exception=HTTPStatus.UNAUTHORIZED)
+def temp_upload(filename: str):
+    return send_from_directory(
+        directory=get_temp_upload_directory().relative_to(Path(current_app.root_path)),
+        path=filename
+    )
